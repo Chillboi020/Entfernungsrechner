4a2e792257cf5966c337cc119d8513e7cb92b12c
ekorn <edwin.korn@edvschule-plattling.de>
2023-09-26 15:12:15 +0200

    Schritt 1 Progress (fast fertig)

--- src/main/java/de/edvschuleplattling/ekorn/hyperflight/HelloController.java
+++ src/main/java/de/edvschuleplattling/ekorn/hyperflight/HelloController.java
@@ -28,24 +28,45 @@ public class HelloController {
         boolean calcCheck = false;
 
         // Geht alle möglichen Fehlerprüfungen durch
-        if (Objects.equals(start, end)) {
+        if (Objects.equals(start, "") && Objects.equals(end, "")) {
+            // System.out.println("Both Empty");
+            lblOutput.setText("Bitte Bahnhöfe eingeben!");
+        } else if (Objects.equals(start, "")) {
+            // System.out.println("Start Empty");
+            lblOutput.setText("Bitte Startbahnhof eingeben!");
+        } else if (Objects.equals(end, "")) {
+            // System.out.println("End Empty");
+            lblOutput.setText("Bitte Endbahnhof eingeben!");
+        } else if (Objects.equals(start, end)) {
+            // System.out.println("Identical");
             lblOutput.setText("Die Bahnhöfe sind gleich!");
-        }
-        else {
+        } else {
             if (!startCheck && !endCheck) {
+                // System.out.println("Both Not Exist");
                 lblOutput.setText("Die Bahnhöfe existieren nicht!");
             } else if (!startCheck) {
+                // System.out.println("Start Not Exist");
                 lblOutput.setText("Startbahnhof existiert nicht!");
             } else if (!endCheck) {
+                // System.out.println("End Not Exist");
                 lblOutput.setText("Zielbahnhof existiert nicht!");
             } else {
+                // wenn keine Fehler vorkommen
                 calcCheck = true;
             }
         }
 
+        // Die Entfernungsberechnung
         if (calcCheck) {
             int distance = EntfernungsRechner.getDistance(start, end);
-            lblOutput.setText(Integer.toString(distance) + "km");
+
+            if (distance == 0) {
+                // System.out.println("No Connection");
+                lblOutput.setText("Es gibt keine Verbindung!");
+            } else {
+                // System.out.println("Success");
+                lblOutput.setText("Von " + start + " nach " + end + ": " + Integer.toString(distance) + "km");
+            }
         }
     }
 }
